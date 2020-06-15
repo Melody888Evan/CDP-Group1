6c5c113e0e2d1c0a25fce555be34fed185038b9e
Zhuoqun Zhang <zhuoqunzhang.e@gmail.com>
2020-06-16 04:53:02 +0800

    change c0

---
+++ category.sql
@@ -0,0 +1,13 @@
+/*
+-- Query: SELECT * FROM meal_order.category
+LIMIT 0, 1000
+
+-- Date: 2020-06-14 16:04
+*/
+INSERT INTO `` (`cid`,`name`) VALUES ('0','All');
+INSERT INTO `` (`cid`,`name`) VALUES ('1','Staple');
+INSERT INTO `` (`cid`,`name`) VALUES ('2','Combo');
+INSERT INTO `` (`cid`,`name`) VALUES ('3','Noddles');
+INSERT INTO `` (`cid`,`name`) VALUES ('4','Beverages ');
+INSERT INTO `` (`cid`,`name`) VALUES ('5','Refreshments');
+INSERT INTO `` (`cid`,`name`) VALUES ('6','Stew');
